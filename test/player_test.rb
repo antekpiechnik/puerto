e7bad6b458d350430e1a3f7b0ce561f67784211a
Michal Bugno <michal.bugno@gmail.com>
2010-01-11 19:05:49 +0100

    Fixed tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,30 +2,30 @@ require 'test/test_helper'
 
 class PlayerTest < Test::Unit::TestCase
   def test_fail_on_too_many_players
-    assert !Player::validates_player_no?(2)
-    assert !Player::validates_player_no?(6)
+    assert !Puerto::Player::validates_player_no?(2)
+    assert !Puerto::Player::validates_player_no?(6)
   end
 
   def test_validates_four_players
-    assert Player::validates_player_no?(4)
+    assert Puerto::Player::validates_player_no?(4)
   end
 
   def test_validates_uniqueness_of_names
-    assert Player::validates_name_uniq?(["Joe", "Foo"], "Silly")
+    assert Puerto::Player::validates_name_uniq?(["Joe", "Foo"], "Silly")
   end
 
   def test_fail_on_identical_name
-    assert !Player::validates_name_uniq?(["Joe"], "Joe")
+    assert !Puerto::Player::validates_name_uniq?(["Joe"], "Joe")
   end
 
   def test_creates_three_named_players
-    @players = Player::create(["Joe", "John", "Jack"])
+    @players = Puerto::Player::create(["Joe", "John", "Jack"])
     assert_equal 3, @players.length
     assert_equal "Joe", @players[0].name
   end
 
   def test_fails_on_two_named_players
-    @players = Player::create(["joe","jack"])
+    @players = Puerto::Player::create(["joe","jack"])
     assert_equal [], @players
   end
 
--- test/puerto_test.rb
+++ test/puerto_test.rb
@@ -2,8 +2,8 @@ require 'test/test_helper'
 
 class PuertoTest < Test::Unit::TestCase
   def test_menu
-    p = Puerto.new
+    p = Puerto::Handlers::Puerto.new
     p.handler.handle(:start)
-    assert p.handler.is_a?(Setup)
+    assert p.handler.is_a?(Puerto::Handlers::Setup)
   end
 end
