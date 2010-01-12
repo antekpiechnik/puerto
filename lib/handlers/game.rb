f66cc97e9b2576c6483f79b9ade336c520252ba3
Michal Bugno <michal.bugno@gmail.com>
2010-01-12 09:23:44 +0100

    Ending game option

--- lib/handlers/game.rb
+++ lib/handlers/game.rb
@@ -18,11 +18,16 @@ class Puerto::Handlers::Game < Puerto::Handlers::BaseHandler
       ["1", ["Show player stats", :player_stats]],
       ["2", ["Show game stats", :game_stats]],
       ["3", ["Help", :help]],
+      ["0", ["End game", :end_game]],
     ]
   end
 
   def player_stats(*args)
     self.assign_handler(Puerto::Handlers::PlayerState.new(self))
+  end
+
+  def end_game(*args)
+    self.assign_handler(@setup)
   end
 
   def title
